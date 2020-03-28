00c88507e1c17d1bdafa842564d31f1a1502b02e
David Gomez <david.gomez@live.com.ar>
2020-03-28 17:55:16 -0300

    Add files via upload

---
+++ ex_1_10_2.py
@@ -0,0 +1,12 @@
+def imprimir_cuadrados():
+    print("Se calcularán cuadrados de números")
+
+    n1 = int(input("Ingrese un número entero: "))
+    n2 = int(input("Ingrese otro número entero: "))
+
+    for x in range(n1, n2):
+        print(f'el cuadrado de {x} es  { x * x}')    
+
+    print("Es todo por ahora")
+
+imprimir_cuadrados()
---
+++ ex_1_10_3.py
@@ -0,0 +1,10 @@
+def imprimir_datos():
+
+    name = n1 = input("Ingrese su nombre: ")
+    print(f'Hola {name}')
+
+    n1 = int(input("Ingrese un número entero: "))
+    n2 = int(input("Ingrese otro número entero: "))
+    print(f'el producto de {n1} y {n2} es  { n1 * n2}') 
+
+imprimir_datos()
