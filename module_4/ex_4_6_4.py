ed0a12dac765675355c20e1c50e492b370aba608
David Gomez <david.gomez@globallogic.com>
2020-04-01 03:24:52 -0300

    Add new exercises of module 4

---
+++ module_4/ex_4_6_4.py
@@ -0,0 +1,58 @@
+'''
+Consigna:
+    Escribir funciones que permitan encontrar:
+    a) El máximo o mínimo de un polinomio de segundo grado (dados los coeficientes a , b y c), indicando si es un máximo o un mínimo.
+    b) Las raíces (reales o complejas) de un polinomio de segundo grado.
+    Nota: validar que las operaciones puedan efectuarse antes de realizarlas (no dividir por cero, ni calcular la raíz de un número negativo).
+    c) La intersección de dos rectas (dadas las pendientes y ordenada al origen de cada recta).
+    Nota: validar que no sean dos rectas con la misma pendiente, antes de efectuar la operación.
+'''
+
+def calular_vertice_x(a, b, c):
+    x = (-1) * (b / (2 * a))
+    return x
+
+def calular_vertice_y(a, b, c, x):
+    y = (a * (x**2)) + (b * x) + c
+    return y
+
+def evalua_raiz(a, b, c):
+    return (b**2) - (4 * a * c)
+
+def calcula_raiz(a, b, c):
+    x1 = (calular_vertice_x(a, b, c) + ((evalua_raiz(a, b, c)** 0.2)/(2*a)))
+    x2 = (calular_vertice_x(a, b, c) - ((evalua_raiz(a, b, c)** 0.2)/(2*a)))
+    return x1, x2 
+
+def vertice(a, b, c):
+    if a == 0:
+        return print('el valor a no puede ser 0')
+    
+    xv = calular_vertice_x(a, b, c)
+    yv = calular_vertice_y(a, b, c, xv)
+
+    if (a > 0):
+        return print('Las coordenadas del vértice mínimo de la función es X = {} | Y = {}'.format( xv, yv ))
+    
+    return print('Las coordenadas del vértice máximo de la función es X = {} | Y = {}'.format(xv, yv ))
+
+def raices(a, b, c):
+    if a == 0:
+        return 'el valor a no puede ser 0'
+    
+    r = evalua_raiz(a, b, c)
+
+    if r < 0:
+        return print('La ecuación posee raíces complejas')
+    elif r == 0:
+        return print('La ecuación posee una raíz doble y es: {}'.format(calular_vertice_x(a, b, c)))
+    else:
+        x1, x2 = calcula_raiz(a, b, c)
+        return print('La ecuación posee raíz doble y son: {} | {}'.format(x1, x2))
+
+def interseccion():
+    return 0
+
+
+#vertice(1, -6, 5)
+raices(2, 4, 3)
