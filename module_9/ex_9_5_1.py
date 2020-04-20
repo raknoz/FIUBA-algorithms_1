0bf31fdb353e01f7fee46d667431cf9ab8f3a275
unknown <David Gomez@SVARLP2018447.ar.softvision.global>
2020-04-19 23:52:43 -0300

    Module 9

---
+++ module_9/ex_9_5_1.py
@@ -0,0 +1,23 @@
+'''
+Consigna:
+    Escribir una función que reciba una lista de tuplas, y que devuelva un diccionario
+    en donde las claves sean los primeros elementos de las tuplas, y los valores una lista con los segundos.
+'''
+
+def tuplas_a_diccionario(l):
+    '''
+        Función que recibe una lista y la convierte en un diccionario.
+        Si la clave existe lo agrega a la misma y sino crea uno nueva.
+    '''
+    _dict = {}
+    for e in l:
+        if e[0] in _dict:
+            lista = _dict.get(e[0])
+            lista.append(e[1])
+            _dict[e[0]] = lista
+        else:
+            _dict[e[0]] = [e[1]]
+
+    return _dict
+
+print(tuplas_a_diccionario([('Hola', 'don Pepito'), ('Hola', 'don Jose'), ('Buenos', 'días')]))
---
+++ module_9/ex_9_5_2.py
@@ -0,0 +1,56 @@
+'''
+Consigna:
+    Diccionarios usados para contar.
+    a) Escribir una función que reciba una cadena y devuelva un diccionario con la cantidad de apariciones de cada palabra en la cadena. 
+    Por ejemplo, si recibe ”Qué lindo día que hace hoy” debe devolver: { 'que': 2, 'lindo': 1, 'día': 1, 'hace': 1, 'hoy': 1}.
+    b) Escribir una función que cuente la cantidad de apariciones de cada caracter en una cadena de texto, y los devuelva en un diccionario.
+    c) Escribir una 
+    función que reciba una cantidad de iteraciones de una tirada de 2 dados a realizar y devuelva la cantidad de veces que se observa cada valor de la suma de los dos
+    dados.
+    Nota: utilizar el módulo random para obtener tiradas aleatorias.
+'''
+import random
+
+def contador_apariciones(l):
+    '''
+        Función que recibe una lista y devuelve un diccionario con el contador de coincidencias.
+    '''
+    _dict = {}
+    for e in l:
+        if e in _dict:
+            _dict[e] = _dict.get(e) + 1
+        else:
+            _dict[e] = 1
+    
+    return _dict
+
+def genera_tirada_dados():
+    '''
+        Genera una lista de combinaciones de dos dados
+    '''
+    tiradas = []
+    for x in range(1, 20):
+        tiradas.append((random.randrange(1, 7), random.randrange(1, 7)))
+
+    return tiradas
+
+def procesa_tirada_dados(l):
+    '''
+        Función que recibe una lista con tupas de tiradas de dados y las suma. 
+        Devolviendo un mapa con las combinaciones
+    '''
+    print(f'Lista de tiradas: {l}')
+    l_suma = []
+    for d1, d2 in l:
+        l_suma.append(d1 + d2)
+
+    print(f'Lista con las sumas: {l_suma}')
+    return contador_apariciones(l_suma)
+
+
+
+#cadena = 'Qué lindo día que hace hoy'
+#print(contador_apariciones(cadena.lower().slipt()))
+#cadena = 'Qué lindo día que hace hoy'
+#print(contador_apariciones(cadena.replace(' ', '').lower()))
+print(procesa_tirada_dados(genera_tirada_dados()))
