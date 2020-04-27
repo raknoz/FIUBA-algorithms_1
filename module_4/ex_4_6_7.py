7effb571e89166316729cdc574f5298465c7699b
unknown <David Gomez@SVARLP2018447.ar.softvision.global>
2020-04-26 23:02:37 -0300

    Code review

---
+++ module_4/ex_4_6_7.py
@@ -0,0 +1,16 @@
+'''
+Consigna:
+    Escribir un programa que reciba como entrada un entero representando un año (por ejemplo 751, 1999, o 2158), y muestre por pantalla el mismo año escrito en números
+    romanos.
+'''
+NUMEROS_ROMANOS = {1000:'M', 900:'CM', 500:'D', 100:'C', 90:'XC', 50:'L', 10:'X', 9:'IX', 5:'V', 1:'I'}
+
+def numeros_romanos(num): 
+    n_romano = ''
+    for valor in NUMEROS_ROMANOS.keys():
+        result = num // valor
+        if result > 0:
+            for i in range(result):
+                n_romano += NUMEROS_ROMANOS.get(valor)
+            num = num % valor
+    return n_romano
