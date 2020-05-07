e02438f789639c1a0489cf9b30c2449f6184d65c
unknown <David Gomez@SVARLP2018447.ar.softvision.global>
2020-05-07 01:57:51 -0300

    Add some functions

--- tp_1/soko.py
+++ tp_1/soko.py
@@ -1,3 +1,15 @@
+OESTE = (-1, 0)
+ESTE = (1, 0)
+NORTE = (0, -1)
+SUR = (0, 1)
+
+PARED = '#'
+CAJA = '$' 
+JUGADOR = '@' 
+OBJETIVO = '.'
+OBJ_CAJA = '*'
+OBJ_JUGADOR = '+'
+
 def crear_grilla(desc):
     '''Crea una grilla a partir de la descripción del estado inicial.
 
@@ -22,24 +34,53 @@ def crear_grilla(desc):
         '#####',
     ])
     '''
+    matrix = []
+    for r in desc:
+        row = []
+        for e in r:
+            row.append(e)
+        matrix.append(row)
+    
+    return matrix
 
 def dimensiones(grilla):
     '''Devuelve una tupla con la cantidad de columnas y filas de la grilla.'''
-
+    if len(grilla) == 0:
+        return (0, 0)
+    return len(grilla[0]), len(grilla)
+    
 def hay_pared(grilla, c, f):
     '''Devuelve True si hay una pared en la columna y fila (c, f).'''
+    return PARED == grilla[f][c]
 
 def hay_objetivo(grilla, c, f):
     '''Devuelve True si hay un objetivo en la columna y fila (c, f).'''
+    return grilla[f][c] in (OBJETIVO, OBJ_CAJA, OBJ_JUGADOR)
 
 def hay_caja(grilla, c, f):
     '''Devuelve True si hay una caja en la columna y fila (c, f).'''
+    return  grilla[f][c] in (CAJA, OBJ_CAJA)
 
 def hay_jugador(grilla, c, f):
     '''Devuelve True si el jugador está en la columna y fila (c, f).'''
+    return grilla[f][c] in (JUGADOR, OBJ_JUGADOR)
 
 def juego_ganado(grilla):
     '''Devuelve True si el juego está ganado.'''
+    for r in grilla:
+        if CAJA in r or OBJ_JUGADOR in r:
+            return False
+    return True
+
+def buscar_jugador(grilla):
+    '''
+        Función que devuelve la posición del jugador en la grilla.
+    '''
+    for f in range(1, grilla):
+        for c in range(1, f):
+            if grilla[f][c] in (JUGADOR, OBJ_JUGADOR):
+                return f, c
+
 
 def mover(grilla, direccion):
     '''Mueve el jugador en la dirección indicada.
@@ -59,3 +100,4 @@ def mover(grilla, direccion):
     movimiento efectuado. La grilla recibida NO se modifica; es decir, en caso
     de que el movimiento sea válido, la función devuelve una nueva grilla.
     '''
+    return grilla
