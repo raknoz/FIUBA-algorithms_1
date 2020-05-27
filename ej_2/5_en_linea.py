01bd598ffea709cfa6e5f156c40b65a73e616707
David Gomez <David Gomez@SVARLP2018447.ar.softvision.global>
2020-05-27 02:09:42 -0300

    Add new funcionality

--- ej_2/5_en_linea.py
+++ ej_2/5_en_linea.py
@@ -1,11 +1,19 @@
 from tkinter import *
 import gamelib
 
+#Información del tablero
 CASILLERO_VACIO = ' '
-TOTAL_FILAS = 10
-TOTAL_COLUMNAS = 10
 JUGADOR_X = 'X'
 JUGADOR_O = 'O'
+#Dimensiones del tablero
+TOTAL_FILAS = 10
+TOTAL_COLUMNAS = 10
+BORDE_SUP_PX = 35
+BORDE_INF_PX = 265
+BORDE_DER_PX = 10
+BORDE_IZQ_PX = 290
+ANCHO_CELDA_PX = 28
+ALTO_CELDA_PX = 23
 
 # Inicio funciones propias #
 
@@ -27,7 +35,11 @@ def casillero_vacio(juego, x, y):
 
 def es_posicion_valida(x, y):
     '''Devuelve que los parámetros x e y estén dentro de los límites'''
-    return x < TOTAL_COLUMNAS and y < TOTAL_FILAS
+    return 0 <= x < TOTAL_COLUMNAS and 0 <= y < TOTAL_FILAS
+
+def obtener_posicion(x, y):
+    '''Función que dadas dos coordenadas X e Y, devuelte la posición en fila y columna'''
+    return (y-BORDE_SUP_PX)//ALTO_CELDA_PX, (x-BORDE_DER_PX)//ANCHO_CELDA_PX
 
 # Fin funciones propias #
 
@@ -53,11 +65,26 @@ def juego_actualizar(juego, x, y):
     del tablero; en ese caso determina el nuevo estado del juego y lo
     devuelve.
     """
+    print(f'muestro coordenadas: {x} | {y}')
+    fila, col = obtener_posicion(x, y)
+
+
     return juego
 
 def juego_mostrar(juego):
     """Actualizar la ventana"""
     gamelib.draw_text('5 en línea', 150, 20)
+    gamelib.draw_text(f'Próximo Jugador: {obtener_jugador(juego)}', 150, 280)
+    gamelib.draw_rectangle(10, 35, 290, 265, fill='black', outline='white', width='2')
+    #Lineas Verticales |
+    for c in range(10, 280, 28):
+        #draw_line(x1, y1, x2, y2, **options)
+        gamelib.draw_line(c , 35, c , 265, fill='white', width=2)
+
+    #Linea horizontal -
+    for f in range(35, 265, 23):
+        #draw_line(x1, y1, x2, y2, **options)
+        gamelib.draw_line(10 , f, 290 , f, fill='white', width=2)
 
 def main():
     juego = juego_crear()
