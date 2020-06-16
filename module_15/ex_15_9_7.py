ce8f288a2d2eccf8dc8d831966f50ec0b1f2c5e3
David Gomez <David Gomez@SVARLP2018447.ar.softvision.global>
2020-06-15 23:47:20 -0300

    Add new exercises

--- module_15/ex_15_9_7.py
+++ module_15/ex_15_9_7.py
@@ -1,29 +1,28 @@
 '''
 Consigna:
-    Implementar el método __str__ de ListaEnlazada, para que se genere una salida legible de lo que contiene la lista, similar a las listas de python.
+    Una lista circular es una lista cuyo último nodo está ligado al primero, de modo que es posible 
+    recorrerla infinitamente. Escribir la clase ListaCircular, incluyendo los métodos insert, append, remove y pop.
 '''
 from IteradorListaEnlazada import IteradorListaEnlazada
 
-class ListaEnlazada:
-    '''Modela una lista enlazada.'''
+class ListaCircular():
+    '''Clase que representa la lista circular. '''
+
     def __init__(self):
-        '''Crea una lista enlazada vacía.'''
+        """Crea una lista enlazada vacía."""
         # referencia al primer nodo (None si la lista está vacía)
         self.prim = None
-        # referencia al último nodo (None si la lista está vacía)
+        # referencia al último nodo
         self.ult = None
         # cantidad de elementos de la lista
         self.len = 0
 
     def __iter__(self):
-        return IteradorListaEnlazada(self)
+        return None
 
-    def __str__(self):
-        ''' Imprime el contenido de la lista enlazada en el mismo formato que las lista e pyhon. ''' 
-        l = []
-        for dato in self:
-            l.append(str(dato))
-        return f'[{", ".join(l)}]'
+    def append(self, x):
+        ''' Función que inserta la final de la lista el elemento x. '''
+        self.insert(self.len, x)
     
     def pop(self, i=None):
         ''' Elimina el nodo de la posición i, y devuelve el dato contenido. Si i está fuera de rango, se levanta la excepción IndexError.
@@ -35,10 +34,13 @@ class ListaEnlazada:
         if i < 0 or i >= self.len:
             raise IndexError('Índice fuera de rango')
 
+        dato = None
+
         if i == 0:
             # Caso particular: saltear la cabecera de la lista
             dato = self.prim.dato
             self.prim = self.prim.prox
+            self.ult.prox = self.prim
         else:
             # Buscar los nodos en las posiciones (i-1) e (i)
             n_ant = self.prim
@@ -48,7 +50,8 @@ class ListaEnlazada:
                 n_act = n_ant.prox
             # Guardar el dato y descartar el nodo
             dato = n_act.dato
-            n_ant.prox = n_act.prox      
+            n_ant.prox = n_act.prox
+            self.ult = n_ant
         self.len -= 1
         return dato
 
@@ -56,51 +59,69 @@ class ListaEnlazada:
         ''' Borra la primera aparición del valor x en la lista.Si x no está en la lista, levanta ValueError'''
         
         if self.len == 0:
-            raise ValueError('Lista vacía')
+            raise ValueError("Lista vacía")
 
         if self.prim.dato == x:
         # Caso particular: saltear la cabecera de la lista
             self.prim = self.prim.prox
+            self.ult.prox = self.prim
         else:
         # Buscar el nodo anterior al que contiene a x (n_ant)
             n_ant = self.prim
             n_act = n_ant.prox
-            while n_act is not None and n_act.dato != x:
+            idx = 1
+            while idx < self.len:
+                if n_act.dato == x:
+                    break
                 n_ant = n_act
                 n_act = n_ant.prox
-                if n_act == None:
-                    raise ValueError('El valor no está en la lista.')
+                idx += 1
+                if idx == self.len:
+                    raise ValueError("El valor no está en la lista.")
             # Descartar el nodo
             n_ant.prox = n_act.prox
         self.len -= 1
 
     def insert(self, i, x):
         '''Inserta el elemento x en la posición i. Si la posición es inválida, levanta IndexError'''
+
         if i < 0 or i > self.len:
-            raise IndexError('Posición inválida')
+            raise IndexError("Posición inválida")
         
         nuevo = self._Nodo(x)
         if i == 0:
         # Caso particular: insertar al principio
             nuevo.prox = self.prim
             self.prim = nuevo
+            if self.len == 0:
+                self.ult = nuevo
+            self.ult.prox = nuevo
+        # Caso particular para el último 
+        elif i == self.len:
+            nuevo.prox = self.prim
+            n_ant = self.ult
+            n_ant.prox = nuevo
+            self.ult = nuevo
         else:
         # Buscar el nodo anterior a la posición deseada
             n_ant = self.prim
             for pos in range(1, i):
                 n_ant = n_ant.prox
-            
-            # Intercalar el nuevo nodo
-            nuevo.prox = n_ant.prox
-            n_ant.prox = nuevo
-        
-        if i == self.len or self.len == 0:
-             self.ult = nuevo
+                # Intercalar el nuevo nodo
+                nuevo.prox = n_ant.prox
+                n_ant.prox = nuevo
         self.len += 1
 
-    def append(self, x):
-        ''' Función que se encarga de insertar x en la última posicion de la lista'''
-        self.insert(self.len, x)
+    def imprimir(self):
+        ''' Función que se encarga de imprimir el contenido de la lista. '''
+        n_act = self.prim
+
+        if self.len == 0:
+            print('No hay elementos')
+        else:
+            for e in range(0, self.len):
+                print(f'{n_act.dato} -> {n_act.prox.dato}')
+                n_act = n_act.prox
 
     class _Nodo:
         def __init__(self, dato=None, prox=None):
---
+++ module_15/ex_15_9_8.py
@@ -0,0 +1,6 @@
+'''
+Consigna:
+    Una lista doblemente enlazada es una lista en la cual cada nodo tiene una referencia al anterior además de al próximo 
+    de modo que es posible recorrerla en ambas direcciones. Escribir la clase ListaDobleEnlazada, incluyendo los métodos insert, 
+    append, remove y pop.
+'''
